7ccea3986e3d48abd14c3a21779df4545f853126
DemianBrinville <demian.brinville.p@gmail.com>
2026-05-25 18:44:58 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h2>Este es el sublime título</h2>;<p>Este NO es el sublime título</p>;
+      <button onClick={() => setCount(count + 1)}>add</button>
+      <p>{count}</p>
+      {count <= 10 ? (
+        <p>Este mensaje aparece si count es menor o igual a 10</p>
+      ) : (
+        <p>Este mensaje aparece si count es mayor a 10</p>
+      )}
     </>
   );
 }
